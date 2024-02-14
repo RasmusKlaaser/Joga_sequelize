08664796f0933185b1a0d9c4501c4fcf2b78f52f
Rasmus <rasmus.klaaser@voco.ee>
2024-02-14 14:11:16 +0200

    Got all articles data

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ sequelize
         console.error('Unable to connect to the database', err);
     });
 
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to sequelize application."});
-});
+//using routes and controllers
+const articleRouter = require('./routes/article');
+app.use('/', articleRouter);
+
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
