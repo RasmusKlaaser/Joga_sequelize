473915de0fea841655e9067e1bbe807943c07b39
Rasmus <rasmus.klaaser@voco.ee>
2024-02-21 13:42:03 +0200

    Getted articles tags data

--- controllers/article.js
+++ controllers/article.js
@@ -1,45 +1,51 @@
-// get connection to database ORM object
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize')
+// connect to database
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('mysql://root:password@localhost:3306/joga-sequelize');
 
-// read model data for table represntation
-const models = require('../models')
+// read model data for table representation
+const models = require('../models');
 
 // get all data from table
 const getAllArticles = (req, res) => {
-    models.Article.findAll()
+    models.Articles.findAll()
     .then(articles => {
         console.log(articles)
         return res.status(200).json({ articles });
     })
-    .catch (error => {
+    .catch(error => {
         return res.status(500).send(error.message);
     })
-} 
-
+};
 
 // show article by this slug
 const getArticleBySlug = (req, res) => {
-    models.Article.findOne({
+    models.Articles.findOne({
         where: {
-            slug : req.params.slug
+            slug: req.params.slug
         },
-        include: [{
-            model: models.Author
-        }],  
+        include: [
+            {
+            model: models.Authors
+            },
+            {
+                model: models.Tags,
+                through: {
+                    model: models.ArticleTags
+                }
+            }
+        ],
     })
     .then(article => {
         console.log(article)
         return res.status(200).json({ article });
     })
     .catch (error => {
-    return res.status(500).send(error.message);
+        return res.status(500).send(error.message);
     })
 };
 
-
-//export controller functions
+// exports controller functions
 module.exports = {
     getAllArticles,
     getArticleBySlug
-}; 
+};
