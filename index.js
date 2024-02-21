fd5a7c2d5bde581079f0015bbb2e980c9433cb07
Rasmus <rasmus.klaaser@voco.ee>
2024-02-21 12:50:57 +0200

    correct files

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ sequelize
 const articleRouter = require('./routes/article');
 app.use('/', articleRouter);
 app.use('article', articleRouter)
+app.use('/author', authorRouter);
 
 
 app.listen(3000, () => {
