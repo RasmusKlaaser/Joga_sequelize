dbc076500f436ee6ca942bc0628c7dae51133124
Rasmus <rasmus.klaaser@voco.ee>
2024-02-26 12:56:32 +0200

    Delete article data

--- routes/article.js
+++ routes/article.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const articleController = require('../controllers/article');
+const articleAdminController = require('../controllers/admin/article');
 
 router.get('/', articleController.getAllArticles);
+router.get('/article/:slug', articleController.getArticleBySlug);
+
+router.post('/create', articleAdminController.createArticle);
+router.post('/edit/:id', articleAdminController.updateArticle);
+
+router.delete('/delete/:id', articleAdminController.deleteArticle);
 
 module.exports = router;
