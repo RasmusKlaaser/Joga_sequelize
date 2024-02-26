26d54bd4d25808db1c1d153314e83e1db97639b2
Rasmus <rasmus.klaaser@voco.ee>
2024-02-26 12:57:34 +0200

    Update article data

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
+// parse requests of content-type - application/json
 app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+// connect to database
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize')
+const sequelize = new Sequelize('mysql://root:password@localhost:3306/joga-sequelize');
 
+// testing connection
 sequelize
     .authenticate()
     .then(() => {
-        console.log('Connected to the database');
+        console.log('Connected to the database.');
+
     })
     .catch(err => {
-        console.error('Unable to connect to the database', err);
+        console.log('Unable to connect to the database:', err);
     });
 
-//using routes and controllers
+// using routes and controllers
 const articleRouter = require('./routes/article');
 app.use('/', articleRouter);
-app.use('article', articleRouter)
+app.use('/article', articleRouter);
+app.use('/admin/article', articleRouter);
+
+const authorRouter = require('./routes/author');
 app.use('/author', authorRouter);
 
-
+// listen requests
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
-});
+})
